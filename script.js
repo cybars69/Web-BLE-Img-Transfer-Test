d0b3fb203e7185cef9f2f892cc2a5916a3b159e8
Arsalan <67588625+cybars69@users.noreply.github.com>
2023-02-03 17:43:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -46,6 +46,22 @@ connectButton.addEventListener("click", function(event) {
                     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
                     ctx.drawImage(img, 0, 0);
+                    // // get the scale
+                    // // it is the min of the 2 ratios
+                    // let scale_factor = Math.min(canvas.width / img.width, canvas.height / img.height);
+
+                    // // Lets get the new width and height based on the scale factor
+                    // let newWidth = img.width * scale_factor;
+                    // let newHeight = img.height * scale_factor;
+
+                    // // get the top left position of the image
+                    // // in order to center the image within the canvas
+                    // let x = (canvas.width / 2) - (newWidth / 2);
+                    // let y = (canvas.height / 2) - (newHeight / 2);
+
+                    // // When drawing the image, we have to scale down the image
+                    // // width and height in order to fit within the canvas
+                    // ctx.drawImage(img, x, y, newWidth, newHeight);
 
                     var context = document
                         .getElementById("out-img-test")
@@ -96,7 +112,7 @@ connectButton.addEventListener("click", function(event) {
     });
 
     startTransferButton.addEventListener("click", function(event) {
-        msg("Starting Transsfer");
+        msg("Starting Transfer");
         transferFile(imageData);
 
         // cancelTransfer();
@@ -334,7 +350,7 @@ async function sendFileBlock(fileContents, bytesAlreadySent) {
             bytesRemaining -= blockLength;
             if (bytesRemaining > 0 && isFileTransferInProgress) {
                 msg("File block written - " + bytesRemaining + " bytes remaining");
-                document.getElementById("progress-out").innerHTML = `${(bytesAlreadySent / 15104 * 100).toFixed(2)} %`;
+                document.getElementById("progress-out").innerHTML = `${(bytesAlreadySent / 15000 * 100).toFixed(2)} %`;
                 document.getElementById("progress-out").style.width = `${bytesAlreadySent / 15104 * 100}%`;
                 bytesAlreadySent += blockLength;
                 sendFileBlock(fileContents, bytesAlreadySent);
